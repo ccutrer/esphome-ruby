9e9c1a990910c46d4bbf367afe20805f785484f4
Cody Cutrer <cody@cutrer.us>
2025-07-05 12:59:56 -0600

    use descriptive states based on device classes for binary sensor

--- lib/esphome/entities/binary_sensor.rb
+++ lib/esphome/entities/binary_sensor.rb
@@ -3,6 +3,38 @@
 module ESPHome
   module Entities
     class BinarySensor < Entity
+      DEVICE_CLASSES = Hash.new({ true => "on", false => "off" }.freeze).merge(
+        "battery" => { true => "low", false => "normal" }.freeze,
+        "battery_charging" => { true => "charging", false => "not charging" }.freeze,
+        "carbon_monoxide" => { true => "detected", false => "clear" }.freeze,
+        "cold" => { true => "cold", false => "normal" }.freeze,
+        "connectivity" => { true => "connected", false => "disconnected" }.freeze,
+        "door" => { true => "open", false => "closed" }.freeze,
+        "garage_door" => { true => "open", false => "closed" }.freeze,
+        "gas" => { true => "detected", false => "clear" }.freeze,
+        "heat" => { true => "hot", false => "normal" }.freeze,
+        "light" => { true => "detected", false => "not detected" }.freeze,
+        "lock" => { true => "locked", false => "unlocked" }.freeze,
+        "moisture" => { true => "wet", false => "dry" }.freeze,
+        "motion" => { true => "detected", false => "clear" }.freeze,
+        "moving" => { true => "moving", false => "stopped" }.freeze,
+        "occupancy" => { true => "occupied", false => "not occupied" }.freeze,
+        "opening" => { true => "open", false => "closed" }.freeze,
+        "plug" => { true => "plugged in", false => "unplugged" }.freeze,
+        "power" => { true => "detected", false => "no power" }.freeze,
+        "presence" => { true => "home", false => "away" }.freeze,
+        "problem" => { true => "problem", false => "ok" }.freeze,
+        "running" => { true => "running", false => "not running" }.freeze,
+        "safety" => { true => "unsafe", false => "safe" }.freeze,
+        "smoke" => { true => "detected", false => "clear" }.freeze,
+        "sound" => { true => "detected", false => "not detected" }.freeze,
+        "tamper" => { true => "tampered", false => "clear" }.freeze,
+        "update" => { true => "available", false => "up-to-date" }.freeze,
+        "vibration" => { true => "detected", false => "clear" }.freeze,
+        "window" => { true => "open", false => "closed" }.freeze
+      ).freeze
+
+      include HasDeviceClass
       include HasState
 
       def initialize(_device, list_entities_response)
@@ -12,6 +44,12 @@ module ESPHome
 
       def status?
         @status
+      end
+
+      def formatted_state
+        return super if state.nil?
+
+        DEVICE_CLASSES[device_class][state]
       end
 
       private
